e25a8ada128cd882c4e45f8057ba3d195d2fd8a6
priyadhakshana <151361684+priyadhakshana@users.noreply.github.com>
2023-11-18 14:46:37 +0530

    Added Day1 Assignments

---
+++ FindNumber.java
@@ -0,0 +1,25 @@
+package assignments;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 45;
+		 
+		if (a>0)  {
+			
+			System.out.println("the number is postive");
+		}
+			else {
+			
+			System.out.println("the number is negative");		
+			
+		}
+		
+	
+		
+
+	}
+
+}
---
+++ FireFox.java
@@ -0,0 +1,22 @@
+package assignments;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	   int ReleaseYear = 1998;
+	   String BrowserName ="Firefox";
+	   char BrowserLogo ='f';
+       boolean isVisible =true;
+	   float BrowserVersion = 100.2f;
+	   
+	   
+//	   System.out.println("year: " + ReleaseYear);
+//	   System.out.println("BrowserName: " + BrowserName);
+//	   System.out.println("isVisible: " + isVisible);
+	   System.out.println("year: " + ReleaseYear + "\nBrowserLogo: " + BrowserLogo + "\nBrowserVersion: " + BrowserVersion + "\nBrowserName: " + BrowserName +"\nisVisible: " + isVisible);
+	   
+			  }
+
+}
